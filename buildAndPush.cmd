c36dc23a79e148f4bb85584d10513dc3dc780ab0
za-yangyu <yangyu@zhongan.io>
2018-09-11 20:34:16 +0800

    auto build and commit

---
+++ buildAndPush.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+echo "---start run auto build & push----"
+echo "please make sure are you install gitbash ,node.js and dependency librarys!"
+set base=%cd%
+set source_dir=%base%\dist
+set target_dir=D:\tmp\ddd
+
+start /b /wait npm build ^
+& echo "!!! Will be deleted %target_dir% and copy %source_dir% to %target_dir% !!!" ^
+& pause ^
+cd %target_dir% ^
+& del /q /s %target_dir%\* ^
+& xcopy /s /e /y %source_dir% %target_dir% ^
+& echo "--build sucdess! start commit and push file ---" ^
+& git add . ^
+& git commit -m "auto build and commit" ^
+& git pull ^
+& git push ^
+& pause
