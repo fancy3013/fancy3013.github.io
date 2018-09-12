d792c89bafb41a057d781372a1d87a9beb23b29a
za-yangyu <yangyu@zhongan.io>
2018-09-12 10:13:12 +0800

    auto build and commit

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -5,7 +5,7 @@ source_dir=$base\static
 target_dir=D:\tmp\ddd
 echo "!!! Will be deleted $target_dir and copy $source_dir to $target_dir !!!"
 
-npm run build && echo "success"
+npm run build & echo "success"
 cd $target_dir
 rm -rf $target_dir\*
 cp -r $source_dir\* $target_dir
